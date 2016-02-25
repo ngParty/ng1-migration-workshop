254cc8f639c9162a9d61a4bb6129ad93999464d5
Martin Hochel <hochelmartin@gmail.com>
2016-02-26 00:26:16 +0100

    feat(step-4): load everything via webpack

---
+++ app/bootstrap.ts
@@ -0,0 +1,24 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import './css/app.css';
+import './css/animations.css';
+
+import 'jquery';
+import 'angular';
+import 'angular-route';
+import 'angular-resource';
+import 'angular-animate';
+
+import './js/core/core.module'
+import './js/core/phone.factory'
+import './js/core/checkmark.filter'
+
+import './js/phone_detail/phone_detail.module'
+import './js/phone_detail/phone_detail.controller'
+import './js/phone_detail/phone_detail.component'
+import './js/phone_detail/phone.animation'
+
+import './js/phone_list/phone_list.module'
+import './js/phone_list/phone_list.controller'
+import './js/phone_list/phone_list.component'
+
+import './js/app.module'
